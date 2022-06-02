e857dc4b99a731edf863a1d4ff4bb40ae3b4a975
Taekywong Won <constwon111@gmail.com>
2022-06-02 23:54:33 +0900

    [Add] typeOrm entity

---
+++ src/boards/board.entity.ts
@@ -0,0 +1,14 @@
+import { BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { BoardStatus } from './boards.model';
+
+export class Board extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+  @Column()
+  description: string;
+  @Column()
+  status: BoardStatus;
+}
