5bbdb6f8673b7c930a4ce14cd74b47542673c577
taekywong Won <constwon111@gmail.com>
2022-06-25 04:17:32 +0900

    [feature] auth complete

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
@@ -14,7 +23,16 @@ export class AuthController {
   }
 
   @Post('signin')
-  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@Req() req: Request) {
+    // console.log('req', req);
+    console.log(req.headers);
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { UserRepository } from './user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'Secret1234',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy], // 여기서 jwtStrategy 를 넣어주는건 auth 모듈에서 사용위해
+  exports: [JwtStrategy, PassportModule], // 다른모듈에서 사용 위해
 })
 export class AuthModule {}
