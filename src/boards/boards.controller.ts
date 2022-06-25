d39fb32fb37524569fc212e4b5363ee14cb39c2c
taekywong Won <constwon111@gmail.com>
2022-06-25 18:08:41 +0900

    [feature] boards gto applied authorization

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -7,6 +7,7 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -15,7 +16,10 @@ import { BoardStatus } from './board-status.enum';
 import { createBoardDto } from './dto/create-board.dto';
 import { Board } from './board.entity';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { AuthGuard } from '@nestjs/passport';
+
 @Controller('boards')
+@UseGuards(AuthGuard())
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
   // nestjs
